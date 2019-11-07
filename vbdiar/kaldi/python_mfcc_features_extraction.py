9ac3da9f040c1cbc97b1959fb06285b8ccffec30
Jack Wu <leopardsaga@gmail.com>
2019-11-07 14:55:33 +0800

    Temporarily add 2 feats to meet Kaldi_mfcc_features_extraction API

--- vbdiar/kaldi/python_mfcc_features_extraction.py
+++ vbdiar/kaldi/python_mfcc_features_extraction.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from python_speech_features import mfcc
 from scipy.io import wavfile
-
+import numpy as np
 
 class PythonMFCCFeatureExtraction():
     def __init__(self):
@@ -10,4 +10,7 @@ class PythonMFCCFeatureExtraction():
     def audio2features(self, input_path):
         (rate, sig) = wavfile.read(input_path)
         mfcc_feat = mfcc(sig, rate)
+        # TODO temporarily add 2 feats to meet Kaldi_mfcc_features_extraction API
+        mfcc_feat = np.append(mfcc_feat, [mfcc_feat[-1]], axis=0)
+        mfcc_feat = np.append(mfcc_feat, [mfcc_feat[-1]], axis=0)
         return mfcc_feat
